0dee4028075e75596298b0bbeebe587ca7303d0a
Chatree Srichart <chatree.srichart@gmail.com>
2019-07-01 23:57:02 +0700

    Create tf.sequencial().add(..) method

--- lib/src/tfjs_base.dart
+++ lib/src/tfjs_base.dart
@@ -1,1 +1,44 @@
+@JS('tf')
+library tf;
+
+import "package:js/js.dart";
+
 // TODO: Put public facing types in this file.
+
+@JS()
+@anonymous
+class Layer {
+
+}
+
+@JS()
+@anonymous
+class LayerArgs {
+  List inputShape;
+  int units;
+  bool useBias;
+}
+
+@JS()
+@anonymous
+class Layers {
+  external dynamic dense(LayerArgs args);
+}
+
+@JS()
+@anonymous
+class Model {
+
+}
+
+@JS()
+@anonymous
+class Sequential extends Model {
+  external dynamic add(Layer layer);
+}
+
+@JS('sequential')
+external Sequential sequential();
+
+@JS('layers')
+external Layers get layers;
